6f9c17df4843a8b45de8e5cb00f1fd5df94bb379
hasan ozsoy <hasanozsoy19@gmail.com>
2023-01-08 19:45:28 +0300

    hasan

--- IdeaProjects/batch105Java/src/ders08_Ternary/C03_Ternary.java
+++ IdeaProjects/batch105Java/src/ders08_Ternary/C03_Ternary.java
@@ -3,6 +3,7 @@ package ders08_Ternary;
 public class C03_Ternary {
     public static void main(String[] args) {
         int a = 5;
+        //ternary tek basına kullanılmaz1
         //ternary tek basına kullanılmaz
         String sonuc = a % 2 == 0 ? "cift sayi" : "tek sayi";
         String sonuc2 = a > 10 ? "buyuk sayi" : "" + 2 * a;
