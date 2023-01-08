6af8dfe8189ed33987b40e755f6328dcfc9b2573
hasan ozsoy <hasanozsoy19@gmail.com>
2023-01-08 23:01:08 +0300

    hasan3

--- IdeaProjects/batch105Java/src/gitCodeCalisma/gitCodeCalisma.java
+++ IdeaProjects/batch105Java/src/gitCodeCalisma/gitCodeCalisma.java
@@ -3,6 +3,7 @@ package gitCodeCalisma;
 public class gitCodeCalisma {
     public static void main(String[] args) {
         System.out.println("hasan");
+        System.out.println("1234");
     }
 
 }
